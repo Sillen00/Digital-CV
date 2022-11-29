f9ab4bbb365b744071a175477c269a9feca5d045
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-29 14:30:34 +0100

    made an portfolio.js file

---
+++ portfolio.js
@@ -0,0 +1,61 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  hamburgerMenuToggle();
+  createPortfolioCard(item1);
+}
+
+
+/*
+    <div class="card">
+    <h4 class="card-title">Grott Dykaren</h4>
+    <img src="./images/Grott-dykaren-liten.png" alt="Image of my text based game: Grott dykaren">
+    <div class="card-texts">
+    <p class="card-description">This is a Interactive text based game where you dive with your friend to find some gold when suddenly your friend disapears.</p>
+    <div class="projekt-links">
+    <a href=""><i class="fa-brands fa-github"></i></a>
+    <a href=""><i class="fa-solid fa-link"></i></a>
+    </div>
+    <p class="card-date">November 2022</p>                      
+    </div>
+    </div>
+    */
+
+function createPortfolioCard({
+  title,
+  img,
+  alt,
+  description,
+  gitLink,
+  dirrLink,
+  date,
+}) {
+  const portfolio = document.querySelector(".portfolio-holder");
+  let code = `
+    <div class="card">
+        <h4 class="card-title">${title}</h4>
+        <img src="${img}" alt="${alt}">
+        <div class="card-texts">
+            <p class="card-description">${description}</p>
+            <div class="projekt-links">
+                <a href="${gitLink}"><i class="fa-brands fa-github"></i></a>
+                <a href="${dirrLink}"><i class="fa-solid fa-link"></i></a>
+            </div>
+            <p class="card-date">${date}</p>                      
+        </div>
+    </div>
+`;
+  portfolio.innerHTML += code;
+}
+
+const item1 = {
+  title: "Grottdykaren",
+  img: "./images/Grott-dykaren-liten.png",
+  alt: "Image of my text based game: Grott dykaren",
+  description: "This is a Interactive text based game where you dive with your friend to find some gold when suddenly your friend disapears.",
+  gitLink: "#",
+  dirrLink: "#",
+  date: "November 2022"
+};
+
+
