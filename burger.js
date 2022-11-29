e52f54a80b0dd9165e9eaaaa95b999b748ec8878
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-29 14:16:04 +0100

    Linked to burder and made it own file.js

---
+++ burger.js
@@ -0,0 +1,9 @@
+function hamburgerMenuToggle() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".menu");
+  
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+  }
