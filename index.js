990bed60f39b7c44ba9b0bcef02c719f35b2f166
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-30 21:54:06 +0100

    text writer. fortsätter imorgon.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function main() {
   clickFirstPageScene();
   mobileMenuContentShowRemove();
   hideStartScene();
-  ageInRealTime();
+  // typeWritingText();
 }
 
 function clickFirstPageScene() {
@@ -20,3 +20,57 @@ function hideStartScene() {
     displayHomePage();
   }
 }
+
+// const textDisplay = document.querySelector("#type-writer");
+// const phrases = [
+//   "Pluggar i goa Göteborg",
+//   "Tidigare elmontör",
+//   "Gillar att koda",
+// ];
+// let i = 0;
+// let j = 0;
+// let currentPhrase = [];
+// let isDeleting = false;
+// let isEnd = false;
+
+// function loop() {
+//   isEnd = false
+//   textDisplay.innerHTML = currentPhrase.join("");
+
+//   if (i < phrases.length) {
+//     if (!isDeleting && j <= phrases[i].length) {
+//       currentPhrase.push(phrases[i][j]);
+//       j++;
+//       textDisplay.innerHTML = currentPhrase.join("");
+//     }
+
+//     if (isDeleting && j <= phrases[i].length){
+//       currentPhrase.pop(phrases[i][j]);
+//       j--
+//       textDisplay.innerHTML = currentPhrase.join("");
+//     }
+
+//     if (j == phrases[i].length) {
+//       isEnd = true
+//       isDeleting = true;
+
+//     }
+
+//     if (isDeleting && j === 0){
+//       currentPhrase = []
+//       isDeleting = false
+//       i++
+//       if (i == phrases.length){
+//         i = 0
+//       }
+//     }
+//   }
+//   const spedUp = Math.random() * (80 - 50) + 50
+//   const normalSpeed = Math.random() * (300 - 200) + 200
+//   const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+//   setTimeout(loop, time);
+// }
+// loop();
+/*
+"Jag pluggar i goa Göteborg till Front End Utveckare, <br>tidigare elmontör."
+*/
