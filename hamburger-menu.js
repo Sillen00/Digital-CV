2ab71a41780148caa28efd130ff75f9aa900791a
Simon Bengtsson <sillen.00@hotmail.com>
2022-12-01 15:45:10 +0100

    Hamburger glitch fix

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,11 +1,19 @@
 
 /**Toggle between hamburger och mobile nav menu */
 function mobileMenuContentShowRemove() {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".menu");
-  
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-    });
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", toggleMenu);
+
+  const links = document.querySelectorAll(".menu li");
+  for (const link of links) {
+    link.addEventListener("click", toggleMenu);
   }
+
+
+  function toggleMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+}
--- index.js
+++ index.js
@@ -26,56 +26,58 @@ function hideStartScene() {
   }
 }
 
-// const textDisplay = document.querySelector("#type-writer");
-// const phrases = [
-//   "Pluggar i goa Göteborg",
-//   "Tidigare elmontör",
-//   "Gillar att koda",
-// ];
-// let i = 0;
-// let j = 0;
-// let currentPhrase = [];
-// let isDeleting = false;
-// let isEnd = false;
+function typeWritingText(){
+const textDisplay = document.querySelector("#type-writer");
+const phrases = [
+  "Pluggar i goa Göteborg",
+  "Tidigare elmontör",
+  "Gillar att koda",
+];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
-// function loop() {
-//   isEnd = false
-//   textDisplay.innerHTML = currentPhrase.join("");
+function loop() {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join("");
 
-//   if (i < phrases.length) {
-//     if (!isDeleting && j <= phrases[i].length) {
-//       currentPhrase.push(phrases[i][j]);
-//       j++;
-//       textDisplay.innerHTML = currentPhrase.join("");
-//     }
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
 
-//     if (isDeleting && j <= phrases[i].length){
-//       currentPhrase.pop(phrases[i][j]);
-//       j--
-//       textDisplay.innerHTML = currentPhrase.join("");
-//     }
+    if (isDeleting && j <= phrases[i].length){
+      currentPhrase.pop(phrases[i][j]);
+      j--
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
 
-//     if (j == phrases[i].length) {
-//       isEnd = true
-//       isDeleting = true;
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true;
 
-//     }
+    }
 
-//     if (isDeleting && j === 0){
-//       currentPhrase = []
-//       isDeleting = false
-//       i++
-//       if (i == phrases.length){
-//         i = 0
-//       }
-//     }
-//   }
-//   const spedUp = Math.random() * (80 - 50) + 50
-//   const normalSpeed = Math.random() * (300 - 200) + 200
-//   const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
-//   setTimeout(loop, time);
-// }
-// loop();
+    if (isDeleting && j === 0){
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i == phrases.length){
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 - 50) + 50
+  const normalSpeed = Math.random() * (300 - 200) + 200
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time);
+}
+loop();
+}
 /*
 "Jag pluggar i goa Göteborg till Front End Utveckare, <br>tidigare elmontör."
 */
