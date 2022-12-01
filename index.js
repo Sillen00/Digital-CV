d1567d5035aacd671dea179186c247fb79705f26
Simon Bengtsson <sillen.00@hotmail.com>
2022-12-01 17:13:47 +0100

    typewriting text works and comment on css

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 window.addEventListener("DOMContentLoaded", main);
 
-
 /**Run function when DOM content loaded */
 function main() {
   clickFirstPageScene();
   mobileMenuContentShowRemove();
   hideStartScene();
-  // typeWritingText();
+  typeWritingText();
 }
 
 /**Click anywhere on page to display home page. */
@@ -26,58 +25,54 @@ function hideStartScene() {
   }
 }
 
-function typeWritingText(){
-const textDisplay = document.querySelector("#type-writer");
-const phrases = [
-  "Pluggar i goa Göteborg",
-  "Tidigare elmontör",
-  "Gillar att koda",
-];
-let i = 0;
-let j = 0;
-let currentPhrase = [];
-let isDeleting = false;
-let isEnd = false;
+function typeWritingText() {
+  const textDisplay = document.querySelector("#type-writer");
+  const phrases = [
+    "Pluggar i goa Göteborg",
+    "Tidigare elmontör",
+    "Gillar att koda",
+  ];
+  let i = 0;
+  let j = 0;
+  let currentPhrase = [];
+  let isDeleting = false;
+  let isEnd = false;
 
-function loop() {
-  isEnd = false
-  textDisplay.innerHTML = currentPhrase.join("");
+  function loop() {
+    isEnd = false;
+    textDisplay.innerHTML = currentPhrase.join("");
 
-  if (i < phrases.length) {
-    if (!isDeleting && j <= phrases[i].length) {
-      currentPhrase.push(phrases[i][j]);
-      j++;
-      textDisplay.innerHTML = currentPhrase.join("");
-    }
+    if (i < phrases.length) {
+      if (!isDeleting && j <= phrases[i].length) {
+        currentPhrase.push(phrases[i][j]);
+        j++;
+        textDisplay.innerHTML = currentPhrase.join("");
+      }
 
-    if (isDeleting && j <= phrases[i].length){
-      currentPhrase.pop(phrases[i][j]);
-      j--
-      textDisplay.innerHTML = currentPhrase.join("");
-    }
+      if (isDeleting && j <= phrases[i].length) {
+        currentPhrase.pop(phrases[i][j]);
+        j--;
+        textDisplay.innerHTML = currentPhrase.join("");
+      }
 
-    if (j == phrases[i].length) {
-      isEnd = true
-      isDeleting = true;
+      if (j == phrases[i].length) {
+        isEnd = true;
+        isDeleting = true;
+      }
 
-    }
-
-    if (isDeleting && j === 0){
-      currentPhrase = []
-      isDeleting = false
-      i++
-      if (i == phrases.length){
-        i = 0
+      if (isDeleting && j === 0) {
+        currentPhrase = [];
+        isDeleting = false;
+        i++;
+        if (i == phrases.length) {
+          i = 0;
+        }
       }
     }
+    const spedUp = Math.random() * (80 - 50) + 50;
+    const normalSpeed = Math.random() * (300 - 200) + 200;
+    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+    setTimeout(loop, time);
   }
-  const spedUp = Math.random() * (80 - 50) + 50
-  const normalSpeed = Math.random() * (300 - 200) + 200
-  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
-  setTimeout(loop, time);
+  loop();
 }
-loop();
-}
-/*
-"Jag pluggar i goa Göteborg till Front End Utveckare, <br>tidigare elmontör."
-*/
