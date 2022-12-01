6674947200a859dc3df3dbdf89378fba229663c4
Simon Bengtsson <sillen.00@hotmail.com>
2022-12-01 12:21:01 +0100

    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>
    Splitted ageInRealTime function into multiple functions

--- om-kontakt.js
+++ om-kontakt.js
@@ -1,44 +1,58 @@
 window.addEventListener("DOMContentLoaded", main);
 
-/**
- * Calls hamburger menu and
- */
+/**Start when "om-kontakt" DOM content is loaded. */
 function main() {
     mobileMenuContentShowRemove();
     ageInRealTime();
 }
 
+/**Calculate birthdate and today date to get my age in real time. */
 function ageInRealTime() {
-  const birthDate = new Date("2000-07-25");
-  const birthDateDay = birthDate.getDate();
-  const birthDateMonth = birthDate.getMonth() + 1;
-  const birthDateYear = birthDate.getFullYear();
-  
-  const todayDate = new Date();
-  const todaySeconds = todayDate.getSeconds();
-  const todayMinutes = todayDate.getMinutes();
-  const todayHours = todayDate.getHours();
-  let todayDay = todayDate.getDate();
-  let todayMonth = todayDate.getMonth() + 1;
-  let todayYear = todayDate.getFullYear();
-  let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  
-  if (birthDateDay > todayDay){
-    todayDay = todayDay + month[todayMonth - 1]
-    todayMonth = todayMonth - 1;
+  const birthDate = getMyBirthDate();
+  const todayDate = getTodayDate();
+  let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  if (birthDate.day > todayDate.Day){
+    todayDate.Day = todayDate.Day + monthDays[todayDate.Month - 1]
+    todayDate.Month = todayDate.Month - 1;
   }
 
-  if (birthDateMonth > todayMonth){
-    todayMonth = todayMonth + 12;
-    todayYear = todayYear - 1;
+  if (birthDate.month > todayDate.Month){
+    todayDate.Month = todayDate.Month + 12;
+    todayDate.Year = todayDate.Year - 1;
   }
-  
-  const d = todayDay - birthDateDay;
-  const m = todayMonth - birthDateMonth;
-  const y = todayYear - birthDateYear;
 
-  const pTag = document.getElementById("age-p-tag");
-  pTag.innerHTML = y + "År, " + m + "Månader, " + d + "Dagar, " + todayHours + "Timmar, " + todayMinutes +"Min, " + todaySeconds + "Sek";
+  const day = todayDate.Day - birthDate.day;
+  const month = todayDate.Month - birthDate.month;
+  const year = todayDate.Year - birthDate.year;
+  renderAge(year, month, day, todayDate.Hours, todayDate.Minutes, todayDate.Seconds);
 }
 
+/**Get my birthdate and return */
+function getMyBirthDate() {
+  const birthDate = new Date("2000-07-25"); // T13:37:00.000
+  const day = birthDate.getDate();
+  const month = birthDate.getMonth() + 1;
+  const year = birthDate.getFullYear();
 
+  return { day, month, year };
+}
+
+/**Get todays date and return */
+function getTodayDate(){
+  const todayDate = new Date();
+  const Seconds = todayDate.getSeconds();
+  const Minutes = todayDate.getMinutes();
+  const Hours = todayDate.getHours();
+  let Day = todayDate.getDate();
+  let Month = todayDate.getMonth() + 1;
+  let Year = todayDate.getFullYear();
+
+  return { Seconds, Minutes, Hours, Day, Month, Year}
+}
+
+/**Render out current age in real time on p-tag */
+function renderAge(year, month, day, todayHours, todayMinutes, todaySeconds) {
+  const pTag = document.getElementById("age-p-tag");
+  pTag.innerHTML = year + "År, " + month + "Månader, " + day + "Dagar, " + todayHours + "Timmar, " + todayMinutes +"Min, " + todaySeconds + "Sek";
+}
